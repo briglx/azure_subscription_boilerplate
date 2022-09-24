eaa057d0f89837e86198e5fe2d894cd7debd5b06
Brig Lamoreaux <bklamoreaux@gmail.com>
2022-09-23 22:52:28 -0700

    Restructure file

--- pipelines/management/connectivity.sh
+++ pipelines/management/connectivity.sh
@@ -29,38 +29,68 @@ rg_name=${rg_name:-rg_connectivity}_$rg_region
 
 # create resource group
 echo creating $rg_name in $rg_region
-az group create -n $rg_name -l $rg_region
-
-# Domain VNet
-vnet_domain=vnet-domain-$rg_region
-vnet_domain_prefix='10.0.0.0/16'
-subnet_aad_ds=snet-add-ds # domain - Azure AD Domain Services subnet
-subnet_aad_ds_prefix='10.0.255.0/27'
-subnet_bastion=AzureBastionSubnet
-subnet_bastion_prefix='10.0.255.64/27'
+az group create --name $rg_name --location $rg_region
 
 # create domain vnet
-echo creating domain vnet
-az network vnet create -g $rg_name -n $vnet_domain --address-prefixes $vnet_domain_prefix
-az network vnet subnet create -g $rg_name -n $subnet_aad_ds --vnet-name $vnet_domain --address-prefixes $subnet_aad_ds_prefix
-az network vnet subnet create -g $rg_name -n $subnet_bastion --vnet-name $vnet_domain --address-prefixes $subnet_bastion_prefix
+vnet_name=vnet-domain-$rg_region
+echo creating $vnet_name vnet in $rg_name
+vnet_prefix='10.0.0.0/16'
+az network vnet create -g $rg_name -n $vnet_name --address-prefixes $vnet_prefix
 
-# Development VNet 
-vnet_dev=vnet-dev-$rg_region
-vnet_dev_prefix='10.1.0.0/16'
-subnet_dev=snet-dev
-subnet_dev_prefix='10.1.1.0/24'
-subnet_bastion=AzureBastionSubnet
-subnet_bastion_prefix='10.1.255.64/27'
+# azure ad domain services subnet
+subnet_name=snet-add-ds
+subnet_prefix='10.0.255.0/27'
+az network vnet subnet create -g $rg_name -n $subnet_name --vnet-name $vnet_domain --address-prefixes $subnet_prefix
 
-# create dev vnet
-echo creating dev vnet
-az network vnet create -g $rg_name -n $vnet_dev --address-prefixes $vnet_dev_prefix
-az network vnet subnet create -g $rg_name -n $subnet_dev --vnet-name $vnet_dev --address-prefixes $subnet_dev_prefix
-az network vnet subnet create -g $rg_name -n $subnet_bastion --vnet-name $vnet_dev --address-prefixes $subnet_bastion_prefix
+# aadds bastion subnet
+subnet_name=AzureBastionSubnet
+subnet_prefix='10.0.255.64/27'
+ip_name=aad_bastion_ip
+bastion_name=aad_ds_bastion
+az network vnet subnet create -g $rg_name -n $subnet_name --vnet-name $vnet_domain --address-prefixes $subnet_prefix
+az network public-ip create --resource-group $rg_name --name $ip_name --sku Standard --location $rg_region
+az network bastion create --resource-group $rg_name --name $bastion_name --public-ip-address $ip_name  --vnet-name $vnet_name --location $rg_region
 
-# Create NSG for subnets
-echo creating nsg for subnets
-nsg_dev=dev-nsg
-az network nsg create --name $nsg_dev --resource-group $rg_name
-az network vnet subnet update -g $rg_name -n $subnet_dev --vnet-name $vnet_dev --network-security-group $nsg_dev
+# create development vnet 
+vnet_name=vnet-dev-$rg_region
+echo creating $vnet_name vnet in $rg_name
+vnet_prefix='10.1.0.0/16'
+az network vnet create --resource-group $rg_name --name $vnet_name --address-prefixes $vnet_prefix
+
+# dev bastion subnet
+subnet_name=AzureBastionSubnet
+subnet_prefix='10.1.255.64/27'
+ip_name=dev_bastion_ip
+bastion_name=dev_bastion
+az network vnet subnet create --resource-group $rg_name --name $subnet_name --vnet-name $vnet_name --address-prefixes $subnet_prefix
+az network public-ip create --resource-group $rg_name --name $ip_name --sku Standard --location $rg_region
+az network bastion create --resource-group $rg_name --name $bastion_name --public-ip-address $ip_name  --vnet-name $vnet_name --location $rg_region
+
+# dev subnet
+subnet_name=snet-dev
+subnet_prefix='10.1.1.0/24'
+az network vnet subnet create --resource-group $rg_name --name $subnet_name --vnet-name $vnet_name --address-prefixes $subnet_prefix
+
+
+
+
+
+
+
+# # Create NSG for subnets
+# echo creating nsg for subnets
+# nsg_name=dev-nsg
+# az network nsg create --name $nsg_name --resource-group $rg_name
+# az network vnet subnet update -g $rg_name -n $subnet_name --vnet-name $vnet_name --network-security-group $nsg_name
+
+# function create_vnet() {
+#      local rg_name=${1}
+#      local vnet_name=${2}
+#      local vnet_prefix=${3}
+
+#      echo creating $vnet_name vnet in $rg_name
+#      # vnet_name=vnet-dev-$rg_region
+#      # vnet_prefix='10.1.0.0/16'
+#      az network vnet create -g $rg_name -n $vnet_name --address-prefixes $vnet_prefix
+
+
