669972a702e17ddfa2d809ef194292650e298209
Brig Lamoreaux <bklamoreaux@gmail.com>
2022-09-27 09:37:35 -0700

    Add jumpbox snet

--- pipelines/management/connectivity.sh
+++ pipelines/management/connectivity.sh
@@ -32,33 +32,44 @@ vnet_core_subnet_bastion_name=AzureBastionSubnet
 vnet_core_subnet_bastion_cidr='10.0.255.64/27'
 vnet_core_subnet_bastion_ip_bastion_name=core_bastion_ip
 vnet_core_subnet_bastion_bastion_core_name=core_bastion
+vnet_core_subnet_jump_box_name=snet-jumpbox
+vnet_core_subnet_jump_box_cidr='10.0.1.0/29'
+vnet_core_subnet_firewall_name=snet-firewall
+vnet_core_subnet_firewall_cidr='10.0.1.8/29'
 vnet_core_subnet_management_name=snet-management
-vnet_core_subnet_management_cidr='10.0.1.0/24'
+vnet_core_subnet_management_cidr='10.0.1.16/26'
 
 vnet_dev_name=vnet-dev-$rg_region
 vnet_dev_cidr='10.1.0.0/16'
 vnet_dev_subnet_confluence_name=snet-confluence
-vnet_dev_subnet_confluence_cidr='10.1.0.'
+vnet_dev_subnet_confluence_cidr='10.1.0.0/24'
 
 # connectivity resource group
 echo creating $rg_name in $rg_region
 az group create --name $rg_name --location $rg_region
 
 # Network watcher
-az network watcher configure --resource-group $rg_name --locations $rg_region
+echo creating network watcher in $rg_name
+az network watcher configure --resource-group $rg_name --locations $rg_region --enabled
 
 # create core vnet 
 echo creating $vnet_core_name vnet in $rg_name
 az network vnet create --resource-group $rg_name --name $vnet_core_name --address-prefixes $vnet_core_cidr
 # core bastion subnet
+echo creating subnet $vnet_core_subnet_bastion_name
 az network vnet subnet create --resource-group $rg_name --name $vnet_core_subnet_bastion_name --vnet-name $vnet_core_name --address-prefixes $vnet_core_subnet_bastion_cidr
 az network public-ip create --resource-group $rg_name --name $vnet_core_subnet_bastion_ip_bastion_name --sku Standard --location $rg_region
 az network bastion create --resource-group $rg_name --name $vnet_core_subnet_bastion_bastion_core_name --public-ip-address $vnet_core_subnet_bastion_ip_bastion_name  --vnet-name $vnet_core_name --location $rg_region
+# jumpbox subnet
+echo creating subnet $vnet_core_subnet_jump_box_name
+az network vnet subnet create --resource-group $rg_name --name $vnet_core_subnet_jump_box_name --vnet-name $vnet_core_name --address-prefixes $vnet_core_subnet_jump_box_cidr
 # management subnet
+echo creating subnet $vnet_core_subnet_management_name
 az network vnet subnet create --resource-group $rg_name --name $vnet_core_subnet_management_name --vnet-name $vnet_core_name --address-prefixes $vnet_core_subnet_management_cidr
 
 # create dev vnet 
 echo creating $vnet_dev_name vnet in $rg_name
 az network vnet create --resource-group $rg_name --name $vnet_dev_name --address-prefixes $vnet_dev_cidr
 # confluence subnet
+echo creating subnet $vnet_dev_subnet_confluence_name
 az network vnet subnet create --resource-group $rg_name --name $vnet_dev_subnet_confluence_name --vnet-name $vnet_dev_name --address-prefixes $vnet_dev_subnet_confluence_cidr
